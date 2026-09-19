52b8c33ce0bb2252d959b71220b55a50c3a3ed48
hariharansamy14-debug <hariharansamy14@gmail.com>
2026-09-19 23:26:55 +0530

    Time: 0 ms (100%), Space: 3.9 MB (100%) - LeetHub

---
+++ 1401-circle-and-rectangle-overlapping/1401-circle-and-rectangle-overlapping.go
@@ -0,0 +1,25 @@
+func checkOverlap(radius int, xCenter int, yCenter int, x1 int, y1 int, x2 int, y2 int) bool {
+   
+    closestX := max(x1, min(xCenter, x2))
+    closestY := max(y1, min(yCenter, y2))
+    
+   
+    dx := xCenter - closestX
+    dy := yCenter - closestY
+    
+    return dx*dx + dy*dy <= radius*radius
+}
+
+func min(a, b int) int {
+    if a < b {
+        return a
+    }
+    return b
+}
+
+func max(a, b int) int {
+    if a > b {
+        return a
+    }
+    return b
+}
